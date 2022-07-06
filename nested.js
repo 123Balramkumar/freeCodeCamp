c57a639ca3513cabfbf0a599ab23db282b5ff211
Balram Kumar <balramkgg99@gmail.com>
2022-07-06 13:46:09 +0530

    nested for loop

---
+++ nested.js
@@ -0,0 +1,15 @@
+for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+           console.log("hello") 
+        }
+    }
+    
+    function simpleArraySum(ar) {
+        
+     var sum =0;
+    for(var i=0;i<ar.length;i++){
+      sum = sum+ar[i]; 
+      
+    }
+    console.log(sum); 
+    }
