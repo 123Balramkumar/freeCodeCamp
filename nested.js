8bc445f5e6d5b458fb74d4c0c65763cf399f937d
Balram Kumar <balramkgg99@gmail.com>
2022-07-06 13:58:01 +0530

    sum of an array element

--- nested.js
+++ nested.js
@@ -1,8 +1,4 @@
-for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-           console.log("hello") 
-        }
-    }
+
     
     function simpleArraySum(ar) {
         
@@ -13,3 +9,4 @@ for(i=0;i<3;i++){
     }
     console.log(sum); 
     }
+    simpleArraySum([1,2,3,4,5,6]);
