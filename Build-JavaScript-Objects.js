e2f3b680c70a93d2382bbd93d2a28d2362bf3d76
Balram Kumar <balramkgg99@gmail.com>
2022-07-11 15:59:32 +0530

    build a java script object

---
+++ Build-JavaScript-Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "balram kumar",
+    legs: 2,
+    tails: 0,
+    friends: ["ram", "krishna"],
+    // Only change code above this line
+  };
+  console.log(myDog);
